a2c899b1fb57c1e5fe4197c537729371afcccd4d
3rynis <aehaldy@tutanota.com>
2022-03-25 10:42:17 -0500

    Updates home routes login and style change to main

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,18 +1,19 @@
 const router = require('express').Router();
 
+
 router.get('/', (req, res) => {
-    res.render('login');
+  temp_auth_test = true; // Auth will replace this
+
+  if (!temp_auth_test) {
+    res.redirect('/login');
+    return;
+  }
+
+  res.render('home');
 });
 
-router.get('/home', (req, res) => {
-    temp_auth_test = true; // Auth will replace this
-
-    if (!temp_auth_test) {
-      res.redirect('/');
-      return;
-    }
-  
-    res.render('home');
+router.get('/login', (req, res) => {
+  res.render('login');
 });
 
 module.exports = router;
